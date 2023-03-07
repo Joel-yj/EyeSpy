79a94792d92259cbd4014826dcc04929aa11c89a
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-03-07 13:46:28 +0800

    adding cosmos client

---
+++ app/src/main/java/com/example/hiddeneye/CosmosClientFactory.java
@@ -0,0 +1,21 @@
+package com.example.hiddeneye;
+
+import com.azure.cosmos.ConsistencyLevel;
+import com.azure.cosmos.CosmosClient;
+import com.azure.cosmos.CosmosClientBuilder;
+
+public class CosmosClientFactory {
+
+    private static final String HOST = "";
+    private static final String MASTER_KEY = "";
+
+    private static CosmosClient cosmosClient = new CosmosClientBuilder()
+            .endpoint(HOST)
+            .key(MASTER_KEY)
+            .consistencyLevel(ConsistencyLevel.EVENTUAL)
+            .buildClient();
+
+    public static CosmosClient getCosmosClient() {
+        return cosmosClient;
+    }
+}
