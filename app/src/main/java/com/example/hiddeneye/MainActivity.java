6f831df7771e3b0ae70e672e1aa04e9ae5da5ca7
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-03-13 09:24:53 +0800

    got the table to show up in activity

--- app/src/main/java/com/example/hiddeneye/MainActivity.java
+++ app/src/main/java/com/example/hiddeneye/MainActivity.java
@@ -3,15 +3,31 @@ package com.example.hiddeneye;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hiddeneye.adapters.ItemAdapter;
+import com.example.hiddeneye.models.VideoAttribute;
 import com.example.hiddeneye.Activities.HomeActivity;
 import com.example.hiddeneye.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private ItemAdapter adapter;
+    private RecyclerView recyclerView;
+    private TableLayout tableLayout;
+    private List<VideoAttribute> data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
