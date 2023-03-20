2247a6fcdf31a9fea82e0cbc73933617ef1bc362
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-03-20 13:47:26 +0800

    attempting to go into fragments

--- app/src/main/java/com/example/hiddeneye/MainActivity.java
+++ app/src/main/java/com/example/hiddeneye/MainActivity.java
@@ -3,22 +3,21 @@ package com.example.hiddeneye;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.hiddeneye.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button recyclerviewbtn;
-
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerviewbtn = findViewById(R.id.recyclerviewbtn);
-        recyclerviewbtn.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.recyclerviewbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
