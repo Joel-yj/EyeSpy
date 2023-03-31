20c49fc5e1996645360d2ab92a7fea01f141c0b6
Joel Tan <nyjc39sc.joel@gmail.com>
2023-03-31 16:56:17 +0800

    json to VideoAttribute coversion

--- app/src/main/java/com/example/hiddeneye/Activities/BlobGettingStartedTask.java
+++ app/src/main/java/com/example/hiddeneye/Activities/BlobGettingStartedTask.java
@@ -3,19 +3,22 @@ package com.example.hiddeneye.Activities;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import com.example.hiddeneye.Models.VideoAttribute;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.microsoft.azure.storage.CloudStorageAccount;
 import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlob;
 import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
 import com.microsoft.azure.storage.blob.CloudBlockBlob;
-import com.microsoft.azure.storage.blob.ListBlobItem;
 
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.InvalidKeyException;
+import java.util.ArrayList;
 
 public class BlobGettingStartedTask extends AsyncTask<String, Void, Void> {
 
@@ -40,22 +43,37 @@ public class BlobGettingStartedTask extends AsyncTask<String, Void, Void> {
             CloudBlobContainer container = blobClient.getContainerReference("test");
 
             // Creating blobs
-            CloudBlockBlob blob = container.getBlockBlobReference("test2.json");
-            blob.uploadText("test hello");
+            CloudBlockBlob blob = container.getBlockBlobReference("android_test.json");
+//            blob.uploadText("test hello");
 
-            //iterating through blobs
-            for (ListBlobItem blobItem : container.listBlobs()){
-                if (blobItem instanceof CloudBlob){
-                    CloudBlob blobby = (CloudBlob) blobItem;
-                    blobby.download(Files.newOutputStream(Paths.get("assets" + blobby.getName())));
-                }
+
+            // converting json file from blob into videoattribute model object
+            InputStream inputStream = blob.openInputStream();
+            Gson gson = new Gson();
+            Reader reader = new InputStreamReader(inputStream);
+            Type listType = new TypeToken<ArrayList<VideoAttribute>>(){}.getType();
+            ArrayList<VideoAttribute> videoAttributeList = gson.fromJson(reader,listType);
+
+            // videoattribute objects
+            for (VideoAttribute item : videoAttributeList){
+                System.out.println(item.getVideoPath());
             }
+
+            // binds blob contents to textview
+//            act.outputText(view, blob.downloadText());
+//            act.outputText(view, videoAttribute.getVideoPath());
+
+
+//            //iterating through blobs
+//            for (ListBlobItem blobItem : container.listBlobs()){
+//                if (blobItem instanceof CloudBlob){
+//                    CloudBlob blobby = (CloudBlob) blobItem;
+//                }
+//            }
 
         } catch (URISyntaxException | InvalidKeyException e) {
             throw new RuntimeException(e);
         } catch (StorageException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
             throw new RuntimeException(e);
         }
 //            // Setup the cloud storage account.
