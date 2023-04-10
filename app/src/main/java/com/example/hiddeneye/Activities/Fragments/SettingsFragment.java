21abbfa3d6b58d2712146ca49db749fa21dfcf7b
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-04-10 09:59:51 +0800

    attempt at post/get

--- app/src/main/java/com/example/hiddeneye/Activities/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/hiddeneye/Activities/Fragments/SettingsFragment.java
@@ -1,14 +1,20 @@
 package com.example.hiddeneye.Activities.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.hiddeneye.R;
-import com.example.hiddeneye.Repository.CognitiveSearchRepository;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -36,14 +42,32 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        CognitiveSearchRepository searchRepository = new CognitiveSearchRepository();
-        searchRepository.searchVideos("*");
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        String url = "https://hiddeneyebackend.azurewebsites.net/hello";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("MyApp", "Response" + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("MyApp", "Error" + error.getMessage());
+            }
+        });
+
+
+        queue.add(stringRequest);
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
