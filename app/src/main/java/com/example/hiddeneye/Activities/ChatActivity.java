ffea7b19b128a369a2196e9dd48ad1dd4bccb93d
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-04-25 09:42:15 +0800

    text passed to chatactivity

---
+++ app/src/main/java/com/example/hiddeneye/Activities/ChatActivity.java
@@ -0,0 +1,25 @@
+package com.example.hiddeneye.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.hiddeneye.databinding.ActivityChatBinding;
+
+public class ChatActivity extends AppCompatActivity {
+
+    ActivityChatBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        Intent i = getIntent();
+        String text = i.getStringExtra("prompt");
+        binding.test1.setText(text);
+    }
+}
--- app/src/main/java/com/example/hiddeneye/Activities/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/hiddeneye/Activities/Fragments/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.hiddeneye.Activities.Fragments;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.hiddeneye.Activities.ChatActivity;
 import com.example.hiddeneye.R;
 
 
@@ -31,10 +36,28 @@ public class HomeFragment extends Fragment {
     }
 
     //TODO Implement openAI langchain interface
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        EditText chatbox = view.findViewById(R.id.chatbox);
+
+        chatbox.setOnTouchListener((v, event) -> {
+
+            if (event.getAction() == MotionEvent.ACTION_UP){
+                if(event.getRawX() >= (chatbox.getRight() - chatbox.getCompoundDrawables()[2].getBounds().width())) {
+                    // your action here
+                    Intent i = new Intent(getActivity(),ChatActivity.class);
+                    i.putExtra("prompt",chatbox.getText().toString());
+                    startActivity(i);
+
+                    return true;
+                }
+            }
+            return false;
+        });
 
         return view;
 
